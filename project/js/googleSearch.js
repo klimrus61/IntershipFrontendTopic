3e32e41b6615bcb509b4c94b992f2fbe82d21173
Klim <klimrus61@mail.ru>
2023-12-15 18:46:40 +0300

    refractor: renames

---
+++ project/js/googleSearch.js
@@ -0,0 +1,8 @@
+function googleSearch()
+{
+    const text=document.getElementById("search").value;
+    const cleanQuery = text.replace(" ","+",text);
+    const url='http://www.google.com/search?q='+cleanQuery;
+
+    window.location.href=url;
+}

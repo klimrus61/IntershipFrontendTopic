07794ca3f63d832a522dcb94afd3b1eb4a4e74a6
Klim <klimrus61@mail.ru>
2023-12-18 03:05:09 +0300

    feat: add dinamic filter to left  sidebar

---
+++ project/js/topicsFilter.js
@@ -0,0 +1,82 @@
+let sidebarData = "";
+const topicContainer = document.querySelector(".sidebar-body");
+
+const createTopic = (topicData) => {
+    const { title, articles } = topicData;
+    const topic = document.createElement("div")
+    topic.classList.add("sidebar-inner-nav");
+    topic.innerHTML = `
+        <ul>
+        <p><strong>${title}: </strong></p>
+
+            ${
+                articles.map((article) => {
+                    return `
+                    <li>
+                        ${article}
+                    </li>`
+                })
+                .join("")}
+        </ul>
+    
+    `;
+    topicContainer.append(topic)
+}
+
+
+fetch('js/content/topics.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse JSON data from the response
+  })
+  .then(data => {
+    sidebarData = data
+    for (const topic of data) {
+        createTopic(topic);
+    }
+    console.log(data); // Output the parsed JSON object
+    // Now you can work with the 'data' object as needed
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+
+
+  const resetTopics = () => {
+
+    topicContainer.innerHTML = "";
+  
+    sidebarData.map((topic) => createTopic(topic));
+  
+  }
+  
+
+const filterArticle = document.getElementById("sidebar-filter-input")
+
+const inputHandler = function(e) {
+
+  const inputText = e.target.value.toLowerCase();
+
+
+  topicContainer.innerText = e.target.value;
+  if (inputText === "") {
+    resetTopics()
+  }
+  else {
+      resetTopics()
+      for (const topicTitle of topicContainer.getElementsByTagName("p")) {
+        if (!topicTitle.innerText.toLowerCase().includes(inputText)) {
+          topicTitle.style.display  = "none";
+        }}
+      for (const topicArticle of topicContainer.getElementsByTagName('li')) {
+        if (!topicArticle.innerText.toLowerCase().includes(inputText)) {
+          topicArticle.style.display  = "none";
+      }}
+    }
+}
+
+
+filterArticle.addEventListener("input", inputHandler);
+filterArticle.addEventListener("propertychange", inputHandler);
